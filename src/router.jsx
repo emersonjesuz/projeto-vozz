42e957dd3430af95723fb30e41e87132151af02f
josiemerson <josiemerson2013@gmail.com>
2023-09-24 20:33:59 -0300

    inicio da pagina de entrada

--- src/router.jsx
+++ src/router.jsx
@@ -5,11 +5,15 @@ import {
 } from "react-router-dom";
 import SignIn from "./pages/SignIn";
 import HeaderLayout from "./layouts";
+import OpeningPage from "./pages/OpeningPage";
+import TransiationPage from "./pages/TransitionPage";
 
 export const Router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      <Route path="/" element={<h1>pagina de carregamento</h1>} />
+      <Route path="/" element={<OpeningPage />} />
+      <Route path="transition" element={<TransiationPage />} />
+
       <Route path="access" element={<HeaderLayout />}>
         <Route path="sign-in" element={<SignIn />} />
       </Route>
