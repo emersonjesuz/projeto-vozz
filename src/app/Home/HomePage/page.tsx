8869b4c0460eef91c57c0900a3504b44ae5030ef
keua Xavier <keuaxavierss@gmail.com>
2023-10-31 15:29:10 -0300

    cadastro de publicação e listagem do feed

--- src/app/Home/HomePage/page.tsx
+++ src/app/Home/HomePage/page.tsx
@@ -2,7 +2,7 @@
 import PerfilUser from '../../../components/PerfilUser/index';
 import Publication from '@/components/Publication';
 import Image from 'next/image';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Arrow from '../../../assets/Home/Arrow.svg';
 import Can from '../../../assets/Home/Can.svg';
 import Student from '../../../assets/Home/Icone_Aprender Off.svg';
@@ -15,20 +15,86 @@ import Clip from '../../../assets/Home/clip.svg';
 /* import userHomeProvider from '../../../hooks/userHomeProvider'; */
 import styles from "./styles.module.scss";
 /* import useHome from '@/hooks/Home/useHome'; */
+import Api from '@/connections/api';
 
+type Feed = {
+  id: number
+  profileId: number
+  name: string
+  userName: string
+  photo: string
+  profileChecked: boolean
+  date: string
+  file: string
+  description: string
+  public_likes: number
+  public_comments: number
+}
+
+// let feedData: Feed[];
+
+// const getFeed = async () => {
+//   try {
+//     const response = await Api.get('/feed');
+//     console.log(response.data)
+//     feedData = response.data;
+//   } catch (error) {
+//     console.log(error)
+//   }
+// };
+// getFeed()
 
 export default function HomePage() {
+
+  useEffect(() => {
+    const getFeed = async () => {
+      try {
+        const { data } = await Api.get('/feed');
+        console.log(data)
+        setFeed(data)
+      } catch (error) {
+        console.log(error)
+      }
+    };
+    getFeed()
+  }, []);
+
   /* const { isAtivedPerfilUser } = useHome(); */
   const [isInputFocused, setIsInputFocused] = useState(false);
-
+  const [description, setDescription] = useState('')
+  const [feed, setFeed] = useState<Feed[]>([])
+  console.log(feed)
 
   const handleInputFocus = () => {
     setIsInputFocused(true);
   };
 
   const handleInputBlur = () => {
-    setIsInputFocused(false);
+    setTimeout(() => {
+      setIsInputFocused(false)
+    }, 25)
   };
+
+  const handleSubmitPost = async () => {
+    try {
+      const response = await Api.post('/publications', {
+        profileId: 1,
+        file: '',
+        description
+      })
+
+      setDescription('')
+      feed.unshift(response.data)
+      console.log(feed)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const clear = () => {
+    setFeed([])
+    console.log(feed)
+  }
 
   return (
     <div className={styles["containerHome"]}>
@@ -42,16 +108,40 @@ export default function HomePage() {
         <div className={styles["publication"]}>
           <label>Criar nova publicação</label>
           {/* <textarea cols="30" rows="5" wrap='harp'></textarea> */}
-          <input type="text" placeholder={`${isInputFocused ? "" : 'escrever...'}`} onFocus={handleInputFocus} onBlur={handleInputBlur} className={styles[isInputFocused ? 'selectedInput' : '']} />
+          <input
+            name='description'
+            value={description}
+            onChange={(event) => setDescription(event.target.value)}
+            type="text"
+            placeholder={`${isInputFocused ? "" : 'escrever...'}`}
+            onFocus={handleInputFocus}
+            onBlur={handleInputBlur}
+            className={styles[isInputFocused ? 'selectedInput' : '']}
+          />
           {isInputFocused ? null : <Image src={Pincel} alt='Caneta' className={styles["imagePincel"]} />}
           <div className={`${styles.containerButtonPublication} ${isInputFocused ? styles.selected : ''}`}>
-            <button><Image src={Clip} alt='Clip' /></button>
+            <button><Image src={Clip} alt='Clip' onClick={clear} /></button>
             <button><Image src={Can} alt='Camera' /></button>
-            <button className={styles["btnPublic"]}><Image src={Arrow} alt='Seta' /></button>
+            <button className={styles["btnPublic"]} onClick={handleSubmitPost}><Image src={Arrow} alt='Seta' /></button>
           </div>
         </div>
         {/* Fazer disso abaixo um componente */}
-        <Publication />
+        {feed.map((pub) => (
+          <Publication
+            key={pub.id}
+            id={pub.id}
+            profileId={pub.profileId}
+            name={pub.name}
+            userName={pub.userName}
+            photo={pub.photo}
+            profileChecked={pub.profileChecked}
+            date={pub.date}
+            file={pub.file}
+            description={pub.description}
+            public_likes={pub.public_likes}
+            public_comments={pub.public_comments}
+          />
+        ))}
         {/*  {isAtivedPerfilUser && <PerfilUser />} */}
       </div>
       <div className={styles["footerHome"]}>
--- src/components/Publication/index.tsx
+++ src/components/Publication/index.tsx
@@ -7,26 +7,40 @@ import Comp from '../../assets/Home/RT.svg';
 import Share from '../../assets/Home/Share.svg';
 import Config from '../../assets/Home/BotãoMenu.svg'
 
-export default function Publication() {
+type Feed = {
+    id: number
+    profileId: number
+    name: string
+    userName: string
+    photo: string
+    profileChecked: boolean
+    date: string
+    file: string
+    description: string
+    public_likes: number
+    public_comments: number
+}
+
+export default function Publication({ id, profileId, name, userName, photo, profileChecked, date, file, description, public_likes, public_comments }: Feed) {
 
     return (
         <div className={styles["containerPublish"]}>
             <div className={styles["headerPublish"]}>
-                <Image src={Photo} alt='Foto de quem publicou' />
-                <h2>Isabela Ferreira</h2>
-                <h4>@isaferreira11</h4>
+                <Image src={photo} alt='Foto de quem publicou' />
+                <h2>{name}</h2>
+                <h4>{userName}</h4>
                 <h5>1m</h5>
                 <Image src={Config} alt='Configurações Publicação' />
             </div>
-            <div className={styles["mainPublish"]}>O piso salarial de enfermagem é um reconhecimento mais do que merecido para esses guerreiros do dia a dia. #PL2564</div>
+            <div className={styles["mainPublish"]}>{description}</div>
             <div className={styles["footerPubish"]}>
                 <div>
                     <Image src={Like} alt='Curtir' className={styles["imagePublishUsers"]} />
-                    <h4>0</h4>
+                    <h4>{public_likes}</h4>
                 </div>
                 <div>
                     <Image src={Menssager} alt='Comentar' />
-                    <h4>0</h4>
+                    <h4>{public_comments}</h4>
                 </div>
                 <Image src={Comp} alt='Compartilhar no Perfil' />
                 <Image src={Share} alt='Compartilhar' />
